0bb135dc86061081610512c80e9670935d6437ef
Matthew Spencer <spence3645@gmail.com>
2026-02-27 11:11:07 -0500

    update skills

--- src/data/gamedev-projects.ts
+++ src/data/gamedev-projects.ts
@@ -7,7 +7,7 @@ export const gamedevProjects: Project[] = [
     tagline: 'An addicting idle game where the goal is to grow your candy cutting empire.',
     description: 'An addicting idle game where the goal is to grow your candy cutting empire.',
     previewGif: 'media/gamedev/candy_cutter.gif',
-    techStack: ['Unity', 'C#'],
+    techStack: ['Unity', 'C#', 'iOS', 'Aseprite'],
     liveUrl: 'https://apps.apple.com/us/app/candy-cutter/id6478093548',
     repoUrl: 'https://github.com/spence3645',
     order: 1,
